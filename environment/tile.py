f55b172afa4551a274383baf6f0a00316fd17231
Szymon Kuleczka <37552821+c00lerxo@users.noreply.github.com>
2019-03-22 18:21:35 +0100

    Add occupation instead of table and is_occupied

--- environment/tile.py
+++ environment/tile.py
@@ -1,8 +1,7 @@
 class Tile:
-    def __init__(self, row_index, col_index, table=None, is_occupied=False,
+    def __init__(self, row_index, col_index, occupation=None,
                  is_kitchen_entrance=False):
         self.row_index = row_index
         self.col_index = col_index
-        self.table = table
-        self.is_occupied = is_occupied
+        self.occupation = occupation
         self.is_kitchen_entrance = is_kitchen_entrance
