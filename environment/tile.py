655bd61ba1967e25efb1ed7371c7ddb23015338b
Szymon Kuleczka <37552821+c00lerxo@users.noreply.github.com>
2019-03-24 12:38:36 +0100

    Update tile.py

--- environment/tile.py
+++ environment/tile.py
@@ -3,5 +3,5 @@ class Tile:
                  is_kitchen_entrance=False):
         self.row_index = row_index
         self.col_index = col_index
-        self.waiter = waiter 
+        self.occupation = occupation 
         self.is_kitchen_entrance = is_kitchen_entrance
