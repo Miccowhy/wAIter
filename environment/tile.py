fc14571af4a1a9d3eeecfed30391c7a5af2bec51
Szymon Kuleczka <37552821+c00lerxo@users.noreply.github.com>
2019-03-24 12:34:40 +0100

    Fix tile.py

--- environment/tile.py
+++ environment/tile.py
@@ -1,5 +1,5 @@
 class Tile:
-    def __init__(self, row_index, col_index, waiter=None,
+    def __init__(self, row_index, col_index, occupation=None,
                  is_kitchen_entrance=False):
         self.row_index = row_index
         self.col_index = col_index
