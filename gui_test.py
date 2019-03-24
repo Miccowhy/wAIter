0ca9bf32bb5419fe62351c9a56457778a315de56
Sogii <42997153+Sogii@users.noreply.github.com>
2019-03-24 11:05:00 +0100

    Update gui_test.py

--- gui_test.py
+++ gui_test.py
@@ -1,13 +1,3 @@
-"""
- Example program to show using an array to back a grid on-screen.
- 
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
- 
- Explanation video: http://youtu.be/mdTeqiWyFnc
-"""
 import pygame
 from random import randint
 import threading
