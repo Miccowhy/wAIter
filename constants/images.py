86aba12d59455f6d3af4050fc000bec8f3cb1049
c00lerxo <szymonkuleczka98@gmail.com>
2019-04-21 19:34:35 +0200

    Move graphical assets from constants/tiles to resources/graphics

--- constants/images.py
+++ constants/images.py
@@ -1,12 +1,12 @@
-# Loaded tiles
 import pygame
 import os
+import sys
 
 # Using relative paths
-current_path = os.path.dirname(__file__)
-tiles_path = os.path.join(current_path, 'tiles')
+root_path = os.path.dirname(sys.modules['__main__'].__file__)
+graphics_path = os.path.join(root_path, 'resources/graphics')
 
-WAITER = pygame.image.load(os.path.join(tiles_path, 'waiter.bmp'))
-TABLE = pygame.image.load(os.path.join(tiles_path, 'table.bmp'))
-ENTRANCE = pygame.image.load(os.path.join(tiles_path, 'entrance.bmp'))
-FLOOR = pygame.image.load(os.path.join(tiles_path, 'floor.bmp'))
+WAITER = pygame.image.load(os.path.join(graphics_path, 'waiter.bmp'))
+TABLE = pygame.image.load(os.path.join(graphics_path, 'table.bmp'))
+ENTRANCE = pygame.image.load(os.path.join(graphics_path, 'entrance.bmp'))
+FLOOR = pygame.image.load(os.path.join(graphics_path, 'floor.bmp'))
