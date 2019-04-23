d87d1186f28a7541359496ec0372b55d1a788230
c00lerxo <szymonkuleczka98@gmail.com>
2019-04-23 17:15:23 +0200

    Add sprite sheet handling class

---
+++ gui/spritesheet.py
@@ -0,0 +1,36 @@
+# https://www.pygame.org/wiki/Spritesheet
+# This class handles sprite sheets
+# Note: When calling images_at the rect is the format:
+# (x, y, x + offset, y + offset)
+
+import pygame
+
+
+class Spritesheet(object):
+    def __init__(self, filename):
+        try:
+            self.sheet = pygame.image.load(filename).convert()
+        except pygame.error as message:
+            print('Unable to load spritesheet image:', filename)
+            raise SystemExit(message)
+
+    # Load a specific image from a specific rectangle
+    def image_at(self, rectangle, colorkey=None):
+        rect = pygame.Rect(rectangle)
+        image = pygame.Surface(rect.size).convert()
+        image.blit(self.sheet, (0, 0), rect)
+        if colorkey is not None:
+            if colorkey is -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey, pygame.RLEACCEL)
+        return image
+
+    # Load a whole bunch of images and return them as a list
+    def images_at(self, rects, colorkey=None):
+        return [self.image_at(rect, colorkey) for rect in rects]
+
+    # Load a whole strip of images
+    def load_strip(self, rect, image_count, colorkey=None):
+        tups = [(rect[0]+rect[2]*x, rect[1], rect[2], rect[3])
+                for x in range(image_count)]
+        return self.images_at(tups, colorkey)
