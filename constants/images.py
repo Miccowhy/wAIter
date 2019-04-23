4c8c5c629c2620bdb12112c8065724bec8682a98
c00lerxo <szymonkuleczka98@gmail.com>
2019-04-23 23:56:55 +0200

    Add new sprites and textures, change images path, tile size and tile margin

--- constants/images.py
+++ constants/images.py
@@ -6,7 +6,7 @@ import sys
 root_path = os.path.dirname(sys.modules['__main__'].__file__)
 graphics_path = os.path.join(root_path, 'resources/graphics')
 
-WAITER = pygame.image.load(os.path.join(graphics_path, 'waiter.bmp'))
-TABLE = pygame.image.load(os.path.join(graphics_path, 'table.bmp'))
-ENTRANCE = pygame.image.load(os.path.join(graphics_path, 'entrance.bmp'))
-FLOOR = pygame.image.load(os.path.join(graphics_path, 'floor.bmp'))
+WAITER = pygame.image.load(os.path.join(graphics_path, 'waiter.png'))
+TABLE = pygame.image.load(os.path.join(graphics_path, 'table.png'))
+ENTRANCE = pygame.image.load(os.path.join(graphics_path, 'entrance.png'))
+FLOOR = pygame.image.load(os.path.join(graphics_path, 'floor.png'))
