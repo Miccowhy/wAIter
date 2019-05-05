4e35a14bca1f7f79ddd8834541f8b7a42fd59cde
c00lerxo <szymonkuleczka98@gmail.com>
2019-05-06 01:02:56 +0200

    Add A* pathfinding final version

---
+++ helpers/pathfinding.py
@@ -0,0 +1,77 @@
+# Suppose our current tile is grid[0][1]
+# State is hash, i.e.
+# {'tile': grid[0][1], 'direction': Direction.DOWN}
+# Goal is also a hash, i.e.
+# {'tile': env.grid[2][6], 'direction': Direction.LEFT}
+# Successor function returns successors, which are all possible states,
+# reachable by single action from current state, i.e. (for our example state):
+# {'tile': grid[0][2], 'direction': Direction.DOWN},
+# {'tile': grid[0][1], 'direction': Direction.LEFT} etc...
+
+
+import numpy as np
+from constants.movement import Direction
+
+
+def astar_search(agent, goal):
+    initial_state = {'tile': agent.current_tile, 'direction': agent.direction, 'parent': None}
+    frontier = [initial_state]
+    explored = []
+
+    while frontier:
+        node = frontier.pop(0)
+        if goal_achieved(node, goal):
+            path = [node]
+            while node['parent'] is not None:
+                path.append(node['parent'])
+                node = node['parent']
+            path.reverse()
+            return path
+        explored.append(node)
+
+        for successor in successors(node):
+            if is_first_visit(successor, explored, frontier):
+                frontier.append(successor)
+            # This condition is probably not correct as it doesn't take into account total cost
+            elif successor in frontier and successor['tile'].cost > node['tile'].cost:
+                frontier[frontier.index(successor)] = node
+
+        frontier.sort(key=lambda state:
+                      state['tile'].cost + get_distance(state['tile'].rect, goal['tile'].rect))
+
+
+def is_first_visit(successor, explored, frontier):
+    return (without_parent(successor) not in map_without_parent(explored)
+            and without_parent(successor) not in map_without_parent(frontier))
+
+
+def successors(state):
+    successors = []
+    for dir in Direction:
+        if dir is not state['direction']:
+            successors.append({'tile': state['tile'], 'direction': dir, 'parent': state})
+    active_neighbors = [neighbor for neighbor in
+                        state['tile'].unoccupied_neighbors_by_directions(state['direction'])
+                        if neighbor['direction'] == state['direction']]
+    for neighbor in active_neighbors:
+        neighbor['parent'] = state
+        successors.append(neighbor)
+    return successors
+
+
+def goal_achieved(state, goal):
+    return state['tile'] == goal['tile'] and state['direction'] == goal['direction']
+
+
+def get_distance(a, b):
+    o = np.sum(np.abs(np.subtract(a, b)))
+    print(o)
+    return o
+
+
+def map_without_parent(lis):
+    return list(map(without_parent, lis))
+
+
+def without_parent(successor):
+    return {key: value for key, value in successor.items() if key != 'parent'}
--- walter.py
+++ walter.py
@@ -1,12 +1,12 @@
 import pygame
 import math
+from helpers.pathfinding import astar_search
 from gui.map_renderer import MapRenderer
-from helpers.findpath import find_path
 from environment.restaurant import Restaurant
 from entities.waiter_agent import WaiterAgent
 from constants.colors import BLACK
 from constants.dimensions import WINDOW_SIZE, GRID_WIDTH, GRID_LENGTH
-
+from constants.movement import Direction
 
 pygame.init()
 
@@ -17,10 +17,9 @@ clock = pygame.time.Clock()
 
 env = Restaurant(GRID_WIDTH, GRID_LENGTH)
 agent = WaiterAgent(env.grid[0][0])
-map_renderer = MapRenderer(env, screen, agent)
 env.grid[0][0].occupation = agent
-test_path = [env.grid[0][1], env.grid[0][2], env.grid[0][3], env.grid[0][4], env.grid[1][4],
-             env.grid[2][4], env.grid[1][4], env.grid[0][4], env.grid[0][3]]
+map_renderer = MapRenderer(env, screen, agent)
+
 
 done = False
 while not done:
@@ -31,7 +30,9 @@ while not done:
             pos = pygame.mouse.get_pos()
             column_clicked = math.trunc(pos[0] / 50)
             row_clicked = math.trunc(pos[1] / 50)
-            agent.path = find_path(agent, env.grid, row_clicked, column_clicked)
+            goal = {'tile': env.grid[row_clicked][column_clicked], 'direction': Direction.DOWN}
+            states_sequence = astar_search(agent, goal)
+            agent.path = [state['tile'] for state in states_sequence]
 
     screen.fill(BLACK)
     map_renderer.render()
