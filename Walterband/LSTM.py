253bc35fec24fc201c30e90a5c858e4befa002d0
Sogii <kamil.beker@icloud.com>
2019-05-12 22:14:22 +0200

    changed epochs to 50

--- Walterband/LSTM.py
+++ Walterband/LSTM.py
@@ -113,7 +113,7 @@ def train(model, network_input, network_output):
     )
     callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=200, batch_size=64, callbacks=callbacks_list)
+    model.fit(network_input, network_output, epochs=50, batch_size=64, callbacks=callbacks_list)
 
 if __name__ == '__main__':
     train_network()
