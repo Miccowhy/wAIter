db2e6105bd642a8749ce2d5f0e17d7ef32c90e2e
c00lerxo <szymonkuleczka98@gmail.com>
2019-05-19 22:49:57 +0200

    create conversation constant

---
+++ constants/conversations.py
@@ -0,0 +1,6 @@
+def PIZZA_CONVERSATION(suggested_pizza):
+    return ['Waiter: What can I do for you?',
+            'Customer: I would like to order a pizza,\n but I dont know  which...',
+            'Waiter: Our newest AI technology \nsuggests that you will probably like\n '
+            + suggested_pizza,
+            'Customer: Sounds great!']
