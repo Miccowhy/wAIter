e84e622f423a27d2675cba1da0dea84a40bee9be
Sogii <42997153+Sogii@users.noreply.github.com>
2019-05-20 10:37:16 +0200

    load different weight model

--- Walterband/predict.py
+++ Walterband/predict.py
@@ -65,7 +65,7 @@ def create_network(network_input, n_vocab):
     model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
     # Load the weights to each node
-    model.load_weights('weights-improvement-01-3.4104-bigger.hdf5')
+    model.load_weights('weights.hdf5')
 
     return model
 
